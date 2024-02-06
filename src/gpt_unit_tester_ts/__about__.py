b2d6599232efaea46f090ca6e9b99a771301b689
mhh <mike.hukiewitz@robotter.ai>
2024-02-06 18:20:23 +0100

    Initial commit

---
+++ src/gpt_unit_tester_ts/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present mhh <mike.hukiewitz@robotter.ai>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/gpt_unit_tester_ts/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present mhh <mike.hukiewitz@robotter.ai>
+#
+# SPDX-License-Identifier: MIT
